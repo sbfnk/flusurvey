4524b6d209de993175364de838cc7857c8845263
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2012-07-02 15:19:11 +0100

    no virology

--- cohorts.R
+++ cohorts.R
@@ -1,3 +1,6 @@
+library(data.table)
+library(ggplot2)
+
 co.all.12 <- read.csv("cohorts_all_201112.csv", sep=",", header=T)
 co.country.12 <- read.csv("cohorts_country_201112.csv", sep=",", header=T)
 co.all.12$date <- as.Date(strptime(paste(co.all.12$year, co.all.12$week*7-1,sep=" "),format="%Y %j"))
@@ -29,3 +32,41 @@ co.fever.all <- rbind(co.fever.all.12,co.fever.all.11,co.fever.all.10)
 co.country <- co.country.12
 co.fever.country <- co.fever.country.12
 co.notired.country <- co.notired.country.12
+
+startdate <- as.Date("2011-11-28")
+enddate <- as.Date("2012-04-19")
+virology <- data.table(read.csv("virology_clean.csv", header=T, sep=","))
+influenza <- virology[strain=="Influenza"]
+setnames(influenza, "strain", "variable")
+setnames(influenza, "samples", "value")
+compare <- data.table(rbind(influenza, co.fever.all.12))
+compare$date <- as.Date(compare$date)
+compare[variable=="Influenza"]$value <-
+  compare[variable=="Influenza"]$value *
+  sum(compare[variable=="vaccinated" & date < enddate]$value) /
+  sum(compare[date > startdate & date < enddate &
+              variable=="Influenza"]$value)
+
+png(filename="cohorts.png", width=960)
+ggplot(compare[date > startdate & date < enddate], aes(x=date, y=value,
+  color=variable))+geom_line(lwd=1.5)+scale_y_continuous(name="Weekly incidence (in %)")+theme_bw(30)+scale_x_date(name="")+opts(panel.grid.major=theme_blank(),
+  panel.grid.minor=theme_blank())+scale_color_brewer(palette="Set1")
+dev.off()
+
+pdf(file="cohorts.pdf", width=12, height=6)
+ggplot(compare[date > startdate & date < enddate], aes(x=date, y=value,
+  color=variable))+geom_line(lwd=1.5)+scale_y_continuous(name="Weekly incidence (in %)")+theme_bw(30)+scale_x_date(name="")+opts(panel.grid.major=theme_blank(),
+  panel.grid.minor=theme_blank())+scale_color_brewer(palette="Set1")
+dev.off()
+
+png(filename="cohorts_no_virology.png", width=960)
+ggplot(compare[variable != "Influenza" & date > startdate & date < enddate], aes(x=date, y=value,
+  color=variable))+geom_line(lwd=1.5)+scale_y_continuous(name="Weekly incidence (in %)")+theme_bw(30)+scale_x_date(name="")+opts(panel.grid.major=theme_blank(),
+  panel.grid.minor=theme_blank())+scale_color_brewer(palette="Set1")
+dev.off()
+
+pdf(file="cohorts_no_virology.pdf", width=12, height=6)
+ggplot(compare[variable != "Influenza" & date > startdate & date < enddate], aes(x=date, y=value,
+  color=variable))+geom_line(lwd=1.5)+scale_y_continuous(name="Weekly incidence (in %)")+theme_bw(30)+scale_x_date(name="")+opts(panel.grid.major=theme_blank(),
+  panel.grid.minor=theme_blank())+scale_color_brewer(palette="Set1")
+dev.off()
