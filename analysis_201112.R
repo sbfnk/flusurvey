da75fd70d1cd055b7916d3cdc9dd89e2c3838dcc
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2012-08-17 14:00:04 +0100

    antiviral and GI stuff

--- analysis_201112.R
+++ analysis_201112.R
@@ -159,6 +159,8 @@ setnames(dt, "Q7_3", "visit.medical.service.hospital")
 setnames(dt, "Q7_4", "visit.medical.service.other")
 setnames(dt, "Q7_5", "visit.medical.service.appointment")
 setnames(dt, "Q7b.1", "visit.medical.service.howsoon")
+setnames(dt, "Q9_3", "medication.antiviral")
+setnames(dt, "Q9_4", "medication.antibiotic")
 setnames(dt, "Q10.1", "alter.routine")
 setnames(dt, "Q10c", "howlong.altered")
 setnames(dt, "Q12_multi_row1_col1", "howmany.household.ili")
@@ -214,7 +216,7 @@ setnames(dt, "x", "maxdate")
 dt$symptoms.start <- as.Date(dt$symptoms.start, "%Y-%m-%d")
 dt$week <- format(dt$date, format="%G-%W")
 dt[dt$week=="2011-00"]$week <- "2011-52"
-#dt$weight <- 1/hist(dt$week, breaks=seq(0,52), plot=F)$counts[dt$week]
+dt$weekweight <- 1/table(dt$week)[dt$week]
 dt$birthdate <- as.Date(dt$birthmonth, "%Y/%M/%d")
 
 # more variables to be used later
@@ -482,16 +484,61 @@ names(vaccination.raw.data) <- c("vaccinated","risk","children","agegroup","year
 write.csv(vaccination.raw.data, "cohorts_fever_201112.raw", quote=F, row.names=F)
 
 # GI stuff
-dt$gi <- as.numeric(dt$diarrhoea == "t" | dt$vomiting == "t")
-dt$newgi <- dt$gi
-dt[same==0, newgi := 0]
-r <- ftable(dt[country == "uk"]$week, dt[country=="uk"]$newgi,
+dt$gi.or <- as.numeric(dt$diarrhoea == "t" | dt$vomiting == "t" | dt$nausea == "t")
+dt$gi.and <- as.numeric(dt$diarrhoea == "t" & dt$vomiting == "t" & dt$nausea == "t")
+dt$gi.or.novom <- as.numeric(dt$diarrhoea == "t" | dt$nausea == "t")
+dt$gi.and.novom <- as.numeric(dt$diarrhoea == "t" & dt$nausea == "t")
+
+dt$newgi.or <- dt$gi.or
+dt$newgi.and <- dt$gi.and
+dt$newgi.or.novom <- dt$gi.or.novom
+dt$newgi.and.novom <- dt$gi.and.novom
+
+dt[same==0, newgi.or := 0]
+dt[same==0, newgi.and := 0]
+dt[same==0, newgi.or.novom := 0]
+dt[same==0, newgi.and.novom := 0]
+
+r.or <- ftable(dt[country == "uk"]$week, dt[country=="uk"]$newgi.or,
             row.vars=1)
-gi.raw.data <- data.frame(expand.grid(rev(attr(r, "row.vars"))),
-                                   unclass(r))
-names(gi.raw.data) <- c("Week", "nongi", "gi")
-gi.raw.data$gi.incidence <- gi.raw.data$gi / (gi.raw.data$nongi + gi.raw.data$nongi)
-gi.12 <- gi.raw.data[-c(1:3, 22:26),]
-write.csv(gi.12, "gi_201112.csv", quote=F, row.names=F)
+r.and <- ftable(dt[country == "uk"]$week, dt[country=="uk"]$newgi.and,
+            row.vars=1)
+r.or.novom <- ftable(dt[country == "uk"]$week, dt[country=="uk"]$newgi.or.novom,
+            row.vars=1)
+r.and.novom <- ftable(dt[country == "uk"]$week, dt[country=="uk"]$newgi.and.novom,
+            row.vars=1)
 
+gi.or.raw.data <- data.frame(expand.grid(rev(attr(r.or, "row.vars"))),
+                                   unclass(r.or))
+gi.and.raw.data <- data.frame(expand.grid(rev(attr(r.and, "row.vars"))),
+                                   unclass(r.and))
+gi.or.novom.raw.data <- data.frame(expand.grid(rev(attr(r.or.novom, "row.vars"))),
+                                   unclass(r.or.novom))
+gi.and.novom.raw.data <- data.frame(expand.grid(rev(attr(r.and.novom, "row.vars"))),
+                                   unclass(r.and.novom))
 
+names(gi.or.raw.data) <- c("Week", "nongi", "gi")
+names(gi.and.raw.data) <- c("Week", "nongi", "gi")
+names(gi.or.novom.raw.data) <- c("Week", "nongi", "gi")
+names(gi.and.novom.raw.data) <- c("Week", "nongi", "gi")
+
+gi.or.raw.data$gi.incidence <-
+  gi.or.raw.data$gi / (gi.or.raw.data$nongi + gi.or.raw.data$nongi)
+gi.and.raw.data$gi.incidence <-
+  gi.and.raw.data$gi / (gi.and.raw.data$nongi + gi.and.raw.data$nongi)
+gi.or.novom.raw.data$gi.incidence <-
+  gi.or.novom.raw.data$gi / (gi.or.novom.raw.data$nongi + gi.or.novom.raw.data$nongi)
+gi.and.novom.raw.data$gi.incidence <-
+  gi.and.novom.raw.data$gi / (gi.and.novom.raw.data$nongi + gi.and.novom.raw.data$nongi)
+
+gi.or.12 <- gi.or.raw.data[-c(1:3, 22:26),]
+gi.and.12 <- gi.and.raw.data[-c(1:3, 22:26),]
+gi.or.novom.12 <- gi.or.novom.raw.data[-c(1:3, 22:26),]
+gi.and.novom.12 <- gi.and.novom.raw.data[-c(1:3, 22:26),]
+
+write.csv(gi.or.12, "gi_or_201112.csv", quote=F, row.names=F)
+write.csv(gi.and.12, "gi_and_201112.csv", quote=F, row.names=F)
+write.csv(gi.or.novom.12, "gi_or_novom_201112.csv", quote=F, row.names=F)
+write.csv(gi.and.novom.12, "gi_and_novom_201112.csv", quote=F, row.names=F)
+
+# antibiotic use
