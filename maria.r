3496efaf68e6bbd8fc990990bda302a1012e9c5b
sbfnk <sbfnk@users.noreply.github.com>
2015-06-24 10:33:00 +0100

    stuff for Maria

--- maria.r
+++ maria.r
@@ -1,2 +1,6 @@
 dt <- readRDS("flusurvey_201314.rds")
-dt <- dt[, c(1, 8, 9, 36, 37, 39, seq(66, 72), 123, 124, 127, 129, 130, 132, 133, 135, seq(136, 187)), with = FALSE]
+write.table(dt, "flusurvey_201314.csv", quote = FALSE, sep = ",", row.names = FALSE)
+
+dt <- dt[, c(1, 5, 8, 9, 36, 37, 39, seq(66, 72), 123, 124, 127, 129, 130, 132, 133, 135, seq(136, 187)), with = FALSE]
+
+write.table(dt, "flusurvey_201314_reduced.csv", quote = FALSE, sep = ",", row.names = FALSE)
