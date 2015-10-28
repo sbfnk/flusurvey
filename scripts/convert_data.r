d34c9dfa25280783737a4a848cc8af1b4ce4f89d
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-10-28 12:32:50 +0000

    package updates

---
+++ scripts/convert_data.r
@@ -0,0 +1,24 @@
+## setwd("/path/to/files/)
+
+library('flusurvey')
+
+files <- list.files(pattern = "(background|contact|symptom)_[0-9][0-9].csv")
+
+year_files <- list()
+for (file in files)
+{
+    year <- sub("^[a-z]*_([0-9]*).csv", "\\1", file)
+    survey <- sub("^([a-z]*)_[0-9]*.csv", "\\1", file)
+    if (nchar(year) == 2) year <- paste0("20", year)
+    if (!(year %in% names(year_files))) year_files[[year]] <- list()
+    year_files[[year]][[survey]] <- file
+}
+
+data <- list()
+years <- names(year_files)
+for (year in years)
+{
+    data[[year]] <- read_data(year_files[[year]], year)
+}    
+
+saveRDS(data, file = paste0("flusurvey_raw_", min(years), "_", max(years), ".rds"))
