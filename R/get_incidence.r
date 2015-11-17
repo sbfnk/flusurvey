6b6b14d1c30de5188ccee0bc39712f0d79b6a78f
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-11-17 09:12:28 +0100

    note active reports

--- R/get_incidence.r
+++ R/get_incidence.r
@@ -5,13 +5,13 @@
 ##' @param incidence.columns one or more columns which specificy incidence; default: "ili"
 ##' @param aggregation the timescale of aggregation, by default "week"
 ##' @param denominator what to use as denominator, active members or all reports in a week
-##' @param min.N the minimum denominator, by default 1000
+##' @param min.N the minimum denominator, by default 1
 ##' @param by one or more variables by which to group
 ##' @import data.table
 ##' @return a data.table with the incidence
 ##' @author seb
 ##' @export
-get_incidence <- function(data, incidence.columns = "ili", aggregation = c("week", "day", "month", "year"), denominator = c("active.members", "reports"), min.N = 1000, by = NULL)
+get_incidence <- function(data, incidence.columns = "ili", aggregation = c("week", "day", "month", "year"), denominator = c("active.members", "reports"), min.N = 1, by = NULL)
 {
     aggregation <- match.arg(aggregation)
     denominator <- match.arg(denominator)
@@ -70,15 +70,18 @@ get_incidence <- function(data, incidence.columns = "ili", aggregation = c("week
     ## work out denominator of active members
     if (denominator == "active.members")
     {
-        incidence[, N := apply(incidence, 1, function(x)
-                    {
-                        current.date <- x[[aggregation]]
-                        id_active <- dt[get(columns[["min.date"]]) <= current.date &
-                                        get(columns[["max.date"]]) >= current.date,
+        active_reports <- lapply(unique(incidence[, get(aggregation)]), function(x)
+{
+                        id_active <- dt[get(columns[["min.date"]]) <= x &
+                                        get(columns[["max.date"]]) >= x,
                                         list(reports = .N), by = c(id_column, by)]
-                        id_active[, .N]
-                    })]
-    } else
+                        id_active <- id_active[, .N, by = by]
+                        id_active[, paste(aggregation) := x]
+                        id_active
+})
+        active_reports <- rbindlist(active_reports)
+        incidence <- merge(incidence, active_reports, by = c(aggregation, by))
+        } else
     {
         id_reports <- dt[, list(reports = .N), by = c(columns[["date"]], id_column, by)]
         reports <- id_reports[, .N, by = c(columns[["date"]], by)]
@@ -86,7 +89,7 @@ get_incidence <- function(data, incidence.columns = "ili", aggregation = c("week
         incidence <- merge(incidence, reports, by = c(aggregation, by))
     }
 
-    incidence <- incidence[N > min.N]
+    incidence <- incidence[N >= min.N]
     
     incidence[, incidence := new.cases / N]
 
