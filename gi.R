799e76e165e1f79ef60b2b5a49a960244453bdda
Sebastian Funk <sbfnk@users.noreply.github.com>
2016-01-25 09:21:52 +0000

    packages, variables

--- gi.R
+++ gi.R
@@ -1,4 +1,11 @@
+library('flusurvey')
+library('reshape2')
+library('data.table')
+
 dt <- extract_data(data = "flusurvey_raw_2012_2015.rds")
+dt[, self.gi := as.integer(what.do.you.think == "gastro")]
+dt[, visit.medical.service := as.integer(visit.medical.service.no == "f")]
+dt[, contact.medical.service := as.integer(contact.medical.service.no == "f")]
 dt[, ili.gi := as.integer(gi == 1 & ili == 1)]
 
 vars <- c("ili", "gi", "ili.gi")
@@ -10,5 +17,7 @@ for (var in vars)
 {
     wgi[is.na(get(var)), paste(var) := 0]
 }
+
+
 
 write.table(wgi, "flusurvey_gi.csv", quote = FALSE, sep = ",", row.names = FALSE)
