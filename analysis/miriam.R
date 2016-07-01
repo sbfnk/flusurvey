096e588879a79c918a1ce80f686731d4f03952eb
sbfnk <sbfnk@users.noreply.github.com>
2016-07-01 11:55:20 +0100

    more analysis

--- analysis/miriam.R
+++ analysis/miriam.R
@@ -1,11 +1,14 @@
 library('flusurvey')
 library('readr')
 
-rd12 <- read_data(list(contact = "~/Research/FluSurvey/Data/contact_12.csv"), 2012)
-rd13 <- read_data(list(contact = "~/Research/FluSurvey/Data/contact_13.csv"), 2013)
+rd12 <- read_data(list(background = "~/Research/FluSurvey/Data/background_12.csv", contact = "~/Research/FluSurvey/Data/contact_12.csv"), 2012)
+rd13 <- read_data(list(background = "~/Research/FluSurvey/Data/background_13.csv", contact = "~/Research/FluSurvey/Data/contact_13.csv"), 2013)
 
-contact12 <- rd12$contact[, c("date", setdiff(colnames(rd$contact), c("date", "timestamp"))), with = FALSE]
-contact13 <- rd13$contact[, c("date", setdiff(colnames(rd$contact), c("date", "timestamp"))), with = FALSE]
+rd12 <- lapply(rd12, function(x) {x[, c("date", setdiff(colnames(x), c("date", "timestamp"))), with = FALSE]})
+rd13 <- lapply(rd13, function(x) {x[, c("date", setdiff(colnames(x), c("date", "timestamp"))), with = FALSE]})
 
-write_csv(contact12, "contacts_201213.csv")
-write_csv(contact12, "contacts_201314.csv")
+survey12 <- merge_data(rd12, clean = c("remove.postcodes"))
+survey13 <- merge_data(rd13, clean = c("remove.postcodes"))
+
+write_csv(survey12, "surveys_201213.csv")
+write_csv(survey13, "surveys_201314.csv")
