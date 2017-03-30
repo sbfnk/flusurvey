7647644ebd17592664704e818a869add03eebb1c
sbfnk <sebastian.funk@lshtm.ac.uk>
2017-03-30 08:33:04 +0100

    read in all contacts

--- analysis/contacts.R
+++ analysis/contacts.R
@@ -1,4 +1,4 @@
 library('flusurvey')
 
-dt <- extract_data("flusurvey_raw_2012_2015.rds", years=c(2012, 2013))
-dt_contacts <- extract_data("flusurvey_raw_2012_2015.rds", years=c(2012, 2013), surveys="contact")
+dt <- extract_data("flusurvey_raw_2010_2015.rds", years=2010:2013)
+dt_contacts <- extract_data("flusurvey_raw_2010_2015.rds", years=2010:2013, surveys="contact")
