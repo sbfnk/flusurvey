b6fa2dce491ce22d789516062cb0e3402269af3a
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2018-08-09 14:19:55 +0100

    more vaccination stuff

--- analysis/vaccination.r
+++ analysis/vaccination.r
@@ -5,15 +5,18 @@ dt <- extract_data("flusurvey_raw_2010_2018.rds", years=2012:2018,
                    surveys=c("background", "symptom"),
                    clean=c("remove.first", "remove.bad.symptom.dates",
                            "remove.bad.health.score", "guess.start.dates",
-                           "limit.season", "create.numeric.id", "n.reports",
+                           "limit.season", "n.reports",
                            "unsuccessful.join", "only.symptoms"))
 
+dt %<>% ## transform variables
+    mutate(date.vaccine=as.character(date.vaccine)) %>%
+    mutate(date.vaccine=if_else(date.vaccine == "", NA_character_, date.vaccine)) %>%
+    mutate(date.vaccine=as.Date(date.vaccine)) %>%
+    mutate(season.id=paste(season, participant_id, sep="."))
+
+## create data set for participant numbers
 dbg <- dt %>%
   filter(!duplicated(background.id)) %>%
-  mutate(date.vaccine=as.character(date.vaccine)) %>%
-  mutate(date.vaccine=if_else(date.vaccine == "", NA_character_, date.vaccine)) %>%
-  mutate(date.vaccine=as.Date(date.vaccine)) %>%
-  mutate(season.id=paste(season, participant_id, sep=".")) %>%
   group_by(participant_id, season) %>%
   mutate(nsurvey.season=n()) %>%
   ungroup %>%
@@ -40,3 +43,23 @@ dbg %>%
   summarise(n()) %>%
   remove_rownames
 
+## create data set for analysis
+dta <- dt %>%
+    group_by(participant_id, season) %>%
+    mutate(first.questionnaire=min(date)) %>%
+    filter(row_number()==n()) %>%
+    ungroup %>%
+    mutate(vaccinated=!is.na(date.vaccine),
+           medication=if_else(no.medication=="t", "f", "t")) %>%
+    arrange(participant_id, season) %>%
+    select(participant_id, season, gender, age, region, main.activity, occupation, highest.education, frequent.contact.children, frequent.contact.elderly, frequent.contact.patients, frequent.contact.none, nb.household, nb.household.children, transport, vaccine.this.year, date.vaccine, starts_with("why.vaccine"), starts_with("why.not.vaccine"), vaccine.last.year, medication, pregnant, smoke, first.questionnaire)
+
+dta_pre_phe <- dta %>%
+    filter(season <= 2015)
+
+part_sample <- sample(unique(dta_pre_phe$participant_id), 100)
+
+dta_sample <- dta_pre_phe %>%
+    filter(participant_id %in% part_sample)
+
+write_csv(dta_sample, "flusurvey_sample.csv")
