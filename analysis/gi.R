1e7c64a7b39dee911f78aaa7e140e4bfbee7cd57
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-07-24 17:33:29 +0100

    updated GI analysis

--- analysis/gi.R
+++ analysis/gi.R
@@ -1,148 +1,85 @@
-library(data.table)
+library('flusurvey')
+library('reshape2')
+library('data.table')
 
-dt11 <- readRDS("flusurvey_201011.rds")
-dt12 <- readRDS("flusurvey_201112.rds")
-dt13 <- readRDS("flusurvey_201213.rds")
-dt14 <- readRDS("flusurvey_201314.rds")
+dt <- extract_data(data = "data/flusurvey_raw_2010_2018.rds",
+                   years=2012:2015, surveys=c("background", "symptom"),
+                   age_breaks=c(5, 65))
 
-## merge what we can merge
-join.vertical <- function (...) {
-    x <- list(...)
-    for (i in 1:(length(x) - 1)) {
-        for (j in 2:(i + 1)) {
-            names.diff <- setdiff(names(x[[j - 1]]), names(x[[i + 1]]))
-            if (length(names.diff) > 0) {
-                x[[j - 1]] <- x[[j - 1]][, !names.diff, with = F]
-            }
-        }
-        names.diff.reverse <- setdiff(names(x[[i + 1]]), names(x[[i]]))
-        if (length(names.diff.reverse) > 0) {
-            x[[i + 1]] <- x[[i + 1]][, !names.diff.reverse, with = F]
-        }
-    }
-    res <- do.call("rbind", c(x, list(use.names = T)))
-}
+dt[, self.gi := factor(ifelse(what.do.you.think == "gastro", "t", "f"), levels=c("f", "t"))]
+dt[, self.ili := factor(ifelse(what.do.you.think == "ili", "t", "f"), levels=c("f", "t"))]
+dt[, visit.medical.service := factor(ifelse(visit.medical.service.no == "f", "t",) "f")]
+dt[, gi.think.gi := factor(ifelse(gi == "t" & self.gi == "t", "t", "f"), levels=c("f", "t"))]
+dt[, gi.fever := factor(ifelse(gi == "t" & fever == "t", "t", "f"), levels=c("f", "t"))]
+dt[, gi.noro := factor(ifelse(gi == "t" & (fever == "t" | headache == "t" | muscle.and.or.joint.pain == "t"), "t", "f"), levels=c("f", "t"))]
+dt[, gi.not.ili := factor(ifelse(gi == "t" & ili == "f", "t", "f"), levels=c("f", "t"))]
+dt[, ili.gi := factor(ifelse(gi == "t" & ili == "t", "t", "f"), levels=c("f", "t"))]
+dt[, ili.gi.think.gi := factor(ifelse(ili.gi == "t" & self.gi == "t", "t", "f"), levels=c("f", "t"))]
+dt[, ili.gi.think.ili := factor(ifelse(ili.gi == "t" & self.ili == "t", "t", "f"), levels=c("f", "t"))]
+dt[, gi.visit.hs := factor(ifelse(gi == "t" & visit.medical.service == "t", "t", "f"), levels=c("f", "t"))]
+dt[, ili.gi.visit.hs := factor(ifelse(ili.gi == "t" & visit.medical.service == "t", "t", "f"), levels=c("f", "t"))]
 
-dt <- join.vertical(dt14, dt13, dt12, dt11)
+vars <- c("ili", "gi", "gi.not.ili", "self.gi", "self.ili", "ili.gi", "gi.think.gi", "gi.fever", "gi.noro", "ili.gi", "ili.gi.think.gi", "ili.gi.think.ili", "gi.visit.hs", "ili.gi.visit.hs")
 
-dt$agegroup <- cut(dt$age, breaks=c(0,18,45,65, max(dt$age, na.rm=T)),
-                   include.lowest=T, right=F)
-dt[, week := date2ISOweek(date)]
-dt[, week.date := ISOweek2date(sub("-[1-7]", "-1", week))]
-dt[, symptoms.start.week := date2ISOweek(symptoms.start.date)]
-dt[, symptoms.start.week.date := ISOweek2date(sub("-[1-7]", "-1", symptoms.start.week))]
-dt[is.na(symptoms.start.week.date), symptoms.start.week.date := week.date]
+gi_incidence <- get_incidence(dt, incidence.columns = vars, by = "agegroup")
 
-levels(dt$agegroup) <- c("<18","18-44","45-64","65+")
-
-dt[, gi := as.integer(diarrhoea | vomiting)]
-dt <- dt[!is.na(gi) & !is.na(ili)]
-
-gi.summary <- dt[, list(reports = length(global.id.number),
-                        ili = sum(ili == 1),
-                        gi = sum(gi == 1),
-                        ili.gi = sum(ili == 1 & gi == 1)),
-                 by = list(symptoms.start.week.date, agegroup)]
-setnames(gi.summary, "symptoms.start.week.date", "week.beginning")
-setkey(gi.summary, week.beginning, agegroup)
-gi.summary[, season := year(week.beginning + 182)]
-
-gi.summary.noage <- gi.summary[, list(reports = sum(reports), ili = sum(ili),
-                                      gi = sum(gi), ili.gi = sum(ili.gi)), 
-                               by = list(week.beginning, season)]
-gi.summary.noage <- gi.summary.noage[season > 2010 & season < 2015]
-# cleaning
-for (this.season in unique(gi.summary.noage[, season]))
+for (var in vars)
 {
-    mean.reports <- mean(gi.summary.noage[season == this.season, reports])
-    gi.summary.noage <- gi.summary.noage[!(season == this.season & reports < mean.reports / 2)]
+    gi_incidence[is.na(get(var)), paste(var) := 0]
 }
 
-gi.summary <- gi.summary[week.beginning %in% gi.summary.noage[, week.beginning]]
-gi.summary <- gi.summary[!is.na(agegroup)]
+round(dt[gi == "t" & ili.gi == "f", prop.table(table(what.do.you.think))], 2)
+round(dt[gi == "t" & ili.gi == "t", prop.table(table(what.do.you.think))], 2)
 
-gi.summary.plot <- copy(gi.summary)
-gi.summary.plot[, gi.non.ili := (gi - ili.gi)/ reports]
-gi.summary.plot[, ili := ili/ reports]
-gi.summary.plot[, gi := gi/ reports]
-gi.summary.plot[, ili.gi := NULL]
+## proportion of GI (but not ILI GI) who think they have GI
+gi_incidence[, sum(gi.think.gi - ili.gi.think.gi) / sum(gi - ili.gi)]
+## [1] 0.2895166
 
-mgi <- data.table(melt(gi.summary.plot, id.vars = c("week.beginning", "reports", "season")))
-min.season <- min(mgi[, season])
+gi_incidence[, sum(ili.gi.think.gi) / sum(ili.gi)]
+## [1] 0.1282468
 
-mgi[, week.beginning.season := as.Date(sub("^201[0-9]",
-                            2015 + year(week.beginning) - season,
-                           week.beginning),
-               format = "%Y-%m-%d"), by = 1:nrow(mgi)]
+gi_incidence[, sum(ili.gi.think.ili) / sum(ili.gi)]
+## [1] 0.3961039
 
-p <- ggplot(mgi,
-            aes(x = week.beginning.season, y = value * 100,
-                color = variable))
-p <- p + geom_line(lwd = 1.2)
-p <- p + facet_grid(season ~ ., scales = "free_y")
-p <- p + scale_color_brewer("", palette = "Set1", labels = c("ILI", "GI", "GI without ILI"))
-p <- p + scale_x_date("")
-p <- p + scale_y_continuous("Prevalence (in %)")
-p <- p + theme(legend.position = "top")
-ggsave("gi_seasons.pdf", p)
+gi_incidence[, sum(gi.visit.hs - ili.gi.visit.hs) / sum(gi - ili.gi)]
+## [1] 0.1205866
 
-gi.summary[, season := NULL]
-write.table(gi.summary, "flusurvey_gi.csv", quote = FALSE, sep = ",", row.names = FALSE)
+gi_incidence[, sum(ili.gi.visit.hs) / sum(ili.gi)]
+## [1] 0.1915584
 
-# GI stuff
-## dt$gi.or <- as.numeric(dt$diarrhoea == "t" | dt$vomiting == "t" | dt$nausea == "t")
-## dt$gi.and <- as.numeric(dt$diarrhoea == "t" & dt$vomiting == "t" & dt$nausea == "t")
-## dt$gi.or.novom <- as.numeric(dt$diarrhoea == "t" | dt$nausea == "t")
-## dt$gi.and.novom <- as.numeric(dt$diarrhoea == "t" & dt$nausea == "t")
+write.table(gi_incidence, "flusurvey_gi.csv", quote = TRUE, sep = ",", row.names = FALSE)
 
-## dt$newgi.or <- dt$gi.or
-## dt$newgi.and <- dt$gi.and
-## dt$newgi.or.novom <- dt$gi.or.novom
-## dt$newgi.and.novom <- dt$gi.and.novom
+## new
 
-## dt[same==0, newgi.or := 0]
-## dt[same==0, newgi.and := 0]
-## dt[same==0, newgi.or.novom := 0]
-## dt[same==0, newgi.and.novom := 0]
+library('tidyverse')
 
-## r.or <- ftable(dt[country == "uk"]$week, dt[country=="uk"]$newgi.or,
-##             row.vars=1)
-## r.and <- ftable(dt[country == "uk"]$week, dt[country=="uk"]$newgi.and,
-##             row.vars=1)
-## r.or.novom <- ftable(dt[country == "uk"]$week, dt[country=="uk"]$newgi.or.novom,
-##             row.vars=1)
-## r.and.novom <- ftable(dt[country == "uk"]$week, dt[country=="uk"]$newgi.and.novom,
-##             row.vars=1)
+incidence <- gi_incidence %>%
+  gather(metric, incidence,
+         starts_with("gi"), starts_with("ili"), starts_with("self")) %>%
+  replace_na(list(incidence=0)) %>% 
+  filter(!is.na(agegroup), N>5) %>%
+  mutate(rate=incidence/N)
 
-## gi.or.raw.data <- data.frame(expand.grid(rev(attr(r.or, "row.vars"))),
-##                                    unclass(r.or))
-## gi.and.raw.data <- data.frame(expand.grid(rev(attr(r.and, "row.vars"))),
-##                                    unclass(r.and))
-## gi.or.novom.raw.data <- data.frame(expand.grid(rev(attr(r.or.novom, "row.vars"))),
-##                                    unclass(r.or.novom))
-## gi.and.novom.raw.data <- data.frame(expand.grid(rev(attr(r.and.novom, "row.vars"))),
-##                                    unclass(r.and.novom))
+all <- gi_incidence %>%
+  gather(metric, incidence,
+         starts_with("gi"), starts_with("ili"), starts_with("self")) %>%
+  group_by(week, season, metric) %>%
+  summarise(incidence=sum(incidence, na.rm=TRUE),
+            N=sum(N, na.rm=TRUE)) %>%
+  ungroup %>%
+  filter(N>100) %>% 
+  mutate(rate=incidence/N)
 
-## names(gi.or.raw.data) <- c("Week", "nongi", "gi")
-## names(gi.and.raw.data) <- c("Week", "nongi", "gi")
-## names(gi.or.novom.raw.data) <- c("Week", "nongi", "gi")
-## names(gi.and.novom.raw.data) <- c("Week", "nongi", "gi")
+monthly_incidence <- gi_monthly_incidence %>%
+  gather(metric, incidence,
+         starts_with("gi"), starts_with("ili"), starts_with("self")) %>%
+  replace_na(list(incidence=0)) %>% 
+  filter(!is.na(agegroup), month(month) %in% c(11, 12, 1:3)) %>%
+  mutate(rate=incidence/N)
 
-## gi.or.raw.data$gi.incidence <-
-##   gi.or.raw.data$gi / (gi.or.raw.data$nongi + gi.or.raw.data$nongi)
-## gi.and.raw.data$gi.incidence <-
-##   gi.and.raw.data$gi / (gi.and.raw.data$nongi + gi.and.raw.data$nongi)
-## gi.or.novom.raw.data$gi.incidence <-
-##   gi.or.novom.raw.data$gi / (gi.or.novom.raw.data$nongi + gi.or.novom.raw.data$nongi)
-## gi.and.novom.raw.data$gi.incidence <-
-##   gi.and.novom.raw.data$gi / (gi.and.novom.raw.data$nongi + gi.and.novom.raw.data$nongi)
+dt[start.date.corrected == "f" & !is.na(symptoms.start.date) & !is.na(symptoms.end.date), duration:=symptoms.end.date-symptoms.start.date]
 
-## gi.or.12 <- gi.or.raw.data[-c(1:3, 22:26),]
-## gi.and.12 <- gi.and.raw.data[-c(1:3, 22:26),]
-## gi.or.novom.12 <- gi.or.novom.raw.data[-c(1:3, 22:26),]
-## gi.and.novom.12 <- gi.and.novom.raw.data[-c(1:3, 22:26),]
+write.table(dt[gi=="t"], "flusurvey_gi_raw.csv", quote = TRUE, sep = ",", row.names = FALSE)
 
-## write.csv(gi.or.12, "gi_or_201112.csv", quote=F, row.names=F)
-## write.csv(gi.and.12, "gi_and_201112.csv", quote=F, row.names=F)
-## write.csv(gi.or.novom.12, "gi_or_novom_201112.csv", quote=F, row.names=F)
-## write.csv(gi.and.novom.12, "gi_and_novom_201112.csv", quote=F, row.names=F)
+p <- ggplot(gi_duration, aes(x=duration, y=..density..)) + geom_histogram(binwidth=1) + xlab("Duration in days") + ylab("Proportion")
+save_plot("gi_duration.pdf", p)
