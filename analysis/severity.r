0f6042ed968c521cc6ba5630ae732f583fc5497d
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-08-01 19:52:02 +0100

    severity with/without illness

--- analysis/severity.r
+++ analysis/severity.r
@@ -1,10 +1,16 @@
 library('flusurvey')
+library('data.table')
 
 dt <- extract_data(file.path("data", "flusurvey_raw_2010_2018.rds"), years=2012:2018, surveys=c("background", "symptom"))
-bouts <- bouts_of_illness(dt, symptomatic.only=TRUE)
 
-reduced_bouts <- bouts[, c(1:3, 5:112, 114:136, 138:177, 221, 225)]
+symptom_bouts <- bouts_of_illness(dt, symptomatic.only=TRUE)
+all_bouts <- bouts_of_illness(dt, symptomatic.only=FALSE)
 
-setnames(reduced_bouts, "bout", "episode")
+reduced_symptom_bouts <- symptom_bouts[, c(1:3, 5:112, 114:136, 138:177, 221, 225)]
+reduced_all_bouts <- all_bouts[, c(1:3, 5:112, 114:136, 138:177, 221, 225)]
 
-saveRDS(reduced_bouts, "flusurvey_illness_episodes.rds")
+setnames(reduced_symptom_bouts, "bout", "episode")
+setnames(reduced_all_bouts, "bout", "episode")
+
+saveRDS(reduced_symptom_bouts, "flusurvey_illness_episodes.rds")
+saveRDS(reduced_all_bouts, "flusurvey_all_episodes.rds")
