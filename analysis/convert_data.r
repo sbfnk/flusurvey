80b8262dd86ed02df3903ee94abbeeb2bc92e810
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-10-18 00:30:40 +0100

    convert all data

--- analysis/convert_data.r
+++ analysis/convert_data.r
@@ -1,7 +1,12 @@
 library('readr')
 
 data_dir <- path.expand(file.path("~", "Research", "Flusurvey", "Data"))
-data_files <- list.files(data_dir, "_[0-9][0-9]\\.csv$", full.names=TRUE)
+data_files <- list.files(data_dir, "_[0-9]+\\.csv$", full.names=TRUE)
+
+year_dirs <- list.files(data_dir, "^[0-9]+$", full.names=TRUE, include.dirs=TRUE)
+for (dir in year_dirs) {
+  data_files <- c(data_files, list.files(dir, "_[0-9]+\\.csv$", full.names=TRUE))
+}
 
 year_files <- list()
 
@@ -12,13 +17,15 @@ for (file in data_files) {
     year_files[[year]][[type]] <- file
 }
 
+year_files <- year_files[order(names(year_files))]
+
 sep <- as.list(rep(",", length(year_files)))
 names(sep) <- names(year_files)
 sep[["2010"]] <- ";"
 
 dt <- list()
 for (year in names(year_files)) {
-    dt[[year]] <- read_data(year_files[[year]], year, sep=sep[[year]])
+  dt[[year]] <- read_data(year_files[[year]], year, sep=sep[[year]])
 }
 
 min_year <- min(names(year_files))
