7b3c66804adb3facb396a2e824e753a10a3d1d47
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-10-21 15:50:00 +0100

    cluster-ready model

--- analysis/contacts_model.r
+++ analysis/contacts_model.r
@@ -1,8 +1,33 @@
-
 library('dplyr')
 library('magrittr')
 library('lubridate')
 library('brms')
+library('docopt')
+
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+
+"Script for modelling flusurvey contact data.
+
+Usage: contact_model.r [options]
+
+Options:
+  -t --type=<contact.type>                 contact type
+  -m --model=<model>                       model to use (base, variate, individual)
+  -h --help                                print this help message
+" -> doc
+
+if (!interactive()) {
+  opts <- docopt(doc)
+  if (opts[["help"]])
+  {
+    print(opts)
+    exit()
+  }
+}
+
+type <- opts[["type"]]
+model <- opts[["model"]]
 
 contacts <- readRDS("res/contacts_health.rds")
 
@@ -14,41 +39,38 @@ dt_back_contacts <- contacts %>%
 
 colnames(dt_back_contacts) <- gsub("\\.", "_", colnames(dt_back_contacts))
 
-for (type in c("conversational", "physical"))
-{
-    participants <-
-        data.frame(global_id=unique(dt_back_contacts$global_id)) %>%
-        mutate(new_participant_id=1:n())
+participants <-
+  data.frame(global_id=unique(dt_back_contacts$global_id)) %>%
+  mutate(new_participant_id=1:n())
 
-    contacts <- dt_back_contacts %>%
-        left_join(participants, by="global_id") %>%
-        select(-global_id, contacts=!!sym(type)) %>%
-        rename(global_id=new_participant_id) %>%
-        filter(!is.na(contacts))
+contacts <- dt_back_contacts %>%
+  left_join(participants, by="global_id") %>%
+  select(-global_id, contacts=!!sym(type)) %>%
+  rename(global_id=new_participant_id) %>%
+  filter(!is.na(contacts))
 
-    mean_contacts <- mean(contacts$contacts, na.rm=TRUE)
+options <- list(
+  family=negbinomial,
+  data=contacts,
+  chains=2,
+  cores=2,
+  iter=4000
+)
 
-    random_poisson_model <-
-        brm(formula=contacts ~ 1, data=contacts, family=poisson,
-            cores=2, chains=2, iter=200)
+variables <- c("agegroup", "weekday", "season_month", "month_name", "gender", "nb_household", "nb_household_children", "main_activity", "occupation", "highest_education", "incidence", "health_status", "incidence:health_status", "urban_rural", "work_urban_rural", "enclosed_indoor_space", "public_transport")
 
-    random_negbin_model <-
-      brm(formula=contacts ~ 1, data=contacts, family=negbinomial,
-          cores=2, chains=2, iter=200)
-
-    variate_model <- brm(
-        formula=contacts ~ 1 + agegroup + weekday + season_month + month_name + gender + nb_household + nb_household_children + main_activity + occupation + highest_education + incidence + urban_rural + work_urban_rural + enclosed_indoor_space + public_transport + health_status, data=contacts, family=negbinomial, cores=2, chains=2, iter=200, prior = set_prior(horseshoe())
-    )
-
-    individual_model <- brm(
-        formula=contacts ~ (1 | global_id) + agegroup + weekday + season_month + month_name + gender + nb_household + nb_household_children + main_activity + occupation + highest_education + incidence + health_status + incidence:health_status + urban_rural + work_urban_rural + enclosed_indoor_space + public_transport, data=contacts, family=negbinomial, cores=2, chains=2, iter=4000, prior = set_prior(horseshoe())
-    )
-
-    saveRDS(list(random=random_model,
-                 variate=variate_model,
-                 individual_mu=individual_mu_model,
-                 individual_sigma=individual_sigma_model,
-                 individual=individual_model,
-                 variate_individual_mu=variate_individual_mu_model),
-            paste0(type, "_contact_models.rds"))
+if (model=="random") {
+  formula <- 1
+} else if (model=="variate") {
+  formula <- paste(c(1, variables), collapse = "+")
+} else if (model=="individual") {
+  formula <- paste(c("(1 | global_id)", variables), collapse = "+")
+} else {
+  stop("Unknown model")
 }
+
+options[["formula"]] <- list(formula=as.formula(paste(contacts, formula, sep="~"))
+
+fit <- do.call(brm, options)
+
+saveRDS(fit, file.path("res", paste0(paste(type, model, sep="_"), ".rds"))
