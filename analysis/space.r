d52c6b783893a7c49f528989de7b39f20febd02b
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2020-04-07 23:46:08 +0100

    Space analysis

---
+++ analysis/space.r
@@ -0,0 +1,17 @@
+library("flusurvey")
+
+dt <- extract_data(data = "data/flusurvey_raw_2010_2018.rds",
+                   years = 2012:2018, surveys = c("background", "symptom"),
+                   clean = c("remove.first", "limit.season", "n.reports", "unsuccessful.join", "only.symptoms"))
+
+vars <- c("ili", "ili.self", "ili.fever")
+
+dt[, postcode_area := gsub("[0-9]", "", postcode)]
+dt[, vaccinated := factor(as.integer(vaccine.this.year == "yes"), levels=0:1, labels=c("f", "t"))]
+
+space_incidence <-
+  get_incidence(dt, incidence.columns = vars,
+                aggregation = "season",
+                by = c("postcode_area", "region", "agegroup", "vaccinated"))
+
+readr::write_csv(space_incidence %>% select(-year), "spatial_incidence.csv")
