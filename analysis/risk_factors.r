e539683d93cb3ddddcbb69af6ffa618ca060d7b4
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2020-07-09 13:19:32 +0100

    risk factor update

--- analysis/risk_factors.r
+++ analysis/risk_factors.r
@@ -6,7 +6,7 @@ create <- FALSE
 if (create) {
   dt <- extract_data(file.path("data", "flusurvey_raw_2010_2019.rds"), years=2012:2019, surveys=c("background", "symptom"), clean = c("remove.first", "limit.season", "n.reports", "unsuccessful.join", "only.symptoms"))
 
-  bouts <- bouts_of_illness(dt, symptomatic.only=TRUE)
+  bouts <- bouts_of_illness(dt, symptomatic.only=FALSE)
   saveRDS(bouts, "bouts_risk_factors.rds")
 }
 
@@ -14,11 +14,11 @@ bouts <- readRDS("bouts_risk_factors.rds")
 
 df_reduced <- bouts %>%
   tbl_df() %>%
-  filter(ili.symptoms == "t" | ili == "t" |
-         ili.fever == "t" | ili.self == "t") %>%
   select(participant_id,
          season,
          starts_with("ili"),
+         symptoms.start.date,
+         symptoms.end.date,
          postcode,
          urban.rural,
          age,
@@ -37,7 +37,17 @@ df_reduced <- bouts %>%
          pets.other, 
          starts_with("risk."),
          smoke,
-         transport)
+         transport) %>%
+  mutate(any.ili = (!is.na(ili.symptoms) & ili.symptoms == "t") |
+           (!is.na(ili) & ili == "t") |
+           (!is.na(ili.fever) & ili.fever == "t") |
+           (!is.na(ili.self) & ili.self == "t")) %>%
+  mutate(symptoms.start.date =
+           if_else(any.ili, symptoms.start.date, as.Date(NA_character_)),
+         symptoms.end.date =
+           if_else(any.ili, symptoms.end.date, as.Date(NA_character_))) %>%
+  select(-any.ili) %>%
+  distinct()
 
  saveRDS(df_reduced, "bouts_reduced.rds")
 
@@ -45,35 +55,25 @@ library('dplyr')
 library('readr')
 
 df_reduced <- readRDS("bouts_reduced.rds") %>%
-  mutate(min.health.score=ifelse(is.finite(min.health.score), min.health.score, NA))
 
 write_csv(df_reduced, "flusurvey_episodes.csv")
 
 df_reduced_jenny <- bouts %>%
   tbl_df() %>%
-  filter(season == "2018/19",
+  filter(season == "2019/20",
          ili.symptoms == "t" | ili == "t" |
          ili.fever == "t" | ili.self == "t") %>%
+  mutate(duration = symptoms.end.date - symptoms.start.date,
+         duration = if_else(between(as.integer(duration), 0, 14),
+                            as.integer(duration), NA_integer_)) %>%
   select(participant_id,
-         season,
-         starts_with("ili"),
-         postcode,
-         urban.rural,
          age,
          gender,
-         vaccine.this.year,
-         main.activity,
-         occupation,
-         highest.education,
-         region,
-         frequent.contact.children,
-         nb.household.children,
-         pets.none,
-         pets.dogs,
-         pets.cats,
-         pets.birds,
-         pets.other, 
          starts_with("risk."),
-         smoke,
-         transport)
+         starts_with("ili"),
+         duration,
+         starts_with("contact."),
+         starts_with("visit."))
+
+write_csv(df_reduced_jenny, "flusurvey_hsb.csv")
 
